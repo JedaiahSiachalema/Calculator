b6bce641d0f7ab4effe53b9cc7218193700f7d4f
Billy <mupelwabillt@gmail.com>
2025-11-04 20:53:40 +0200

    I have added multiplication and division

--- utils/operators.py
+++ utils/operators.py
@@ -3,3 +3,10 @@ def output(string):
     print(string)
     return
 
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error!: Division is Undefined"
+    return x / y
